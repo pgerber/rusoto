d2b396fad2664eae078b290120572267065c3d9a
Peter Gerber <peter@arbitrary.ch>
2018-02-17 18:13:55 +0100

    ceph/minio tests: show docker logs

--- integration_tests/docker_test_run.py
+++ integration_tests/docker_test_run.py
@@ -17,8 +17,9 @@ class Docker:
 
     def __enter__(self):
         print("starting container for {!r}".format(self.image))
-        self.container = subprocess.check_output(["docker", "run", "-d", "--rm" ] \
+        self.container = subprocess.check_output(["docker", "run", "-d"] \
             + self.run_opts + [self.image] + self.run_args, universal_newlines=True).strip()
+
         try:
             self._wait_for_s3()
             sys.stdout.flush()
@@ -45,6 +46,16 @@ class Docker:
         if self.container is not None:
             print("terminating docker container {!r} (image {!r})".format(self.container, self.image))
             subprocess.check_call(["docker", "kill", self.container])
+
+        time.sleep(30)
+
+        with subprocess.Popen(["docker", "logs", self.container], universal_newlines=True, stdout=subprocess.PIPE) as proc:
+            prefix = 'docker {}:'.format(self.container[:12])
+            for line in proc.stdout:
+                print(prefix, line)
+
+        subprocess.check_call(['docker', 'rm', self.container])
+
 
 
 def parse_args(args):
@@ -73,6 +84,8 @@ def parse_args(args):
     parser.add_argument("--port", type=int, default=80,
                         help="Port on which the service is going to be listening. Command is only executed once the "
                              "port (default %(default)s) returns a valid HTTP response.")
+    parser.add_argument("--timeout", type=int, default=600, metavar="N",
+                        help="Abort test after N seconds. Defaults to %(default)s seconds.")
     parser.add_argument("command", help="Command to execute.")
     parser.add_argument("args", nargs="*", help="Arguments passed to command. May be given multiple times.")
     return parser.parse_args(args[1:])
@@ -83,7 +96,11 @@ def main():
     rc = 0
     for image in args.docker_images:
         with Docker(image=image, run_opts=args.docker_run_opts, run_args=args.docker_run_args, port=args.port):
-            if subprocess.call([args.command] + args.args) != 0:
+            try:
+                if subprocess.call([args.command] + args.args, timeout=args.timeout) != 0:
+                    rc = 1
+            except subprocess.TimeoutExpired as e:
+                print("ERROR:", e)
                 rc = 1
     return rc
 
