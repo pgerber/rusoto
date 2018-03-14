3ef2ac47db7574959eb8a747046ee8d5c42e29e4
Peter Gerber <peter@arbitrary.ch>
2018-03-15 00:17:02 +0100

    stdout

--- .semaphoreci/run.sh
+++ .semaphoreci/run.sh
@@ -5,4 +5,4 @@ export RUST_BACKTRACE=1
 export PATH=${HOME}/.cargo/bin:${PATH}
 export GIT_ROOT=$(git rev-parse --show-toplevel)
 
-run-parts --verbose --exit-on-error --regex '^test_' .semaphoreci
+run-parts --verbose --exit-on-error --regex '^test_' .semaphoreci 2>&1
